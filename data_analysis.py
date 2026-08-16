e8453719798e558d355f2939cbd752fefebadfa3
Mikail.U <43473825+mikail2004@users.noreply.github.com>
2026-08-15 22:59:59 -0400

    Completed initial notes

--- data_analysis.py
+++ data_analysis.py
@@ -13,6 +13,10 @@ Notes:
     cell is from the average expression of that same gene across all analyzed cells.
 2. Cell cycle checkpoints: stages where a cell checks its health, size, 
     and DNA before moving forward to divide (mitosis).
+3. T-cells get exhausted fighting cancer: the continuous, non-stop presence of tumors (unlike other diseases)
+    overstimulates them. This chronic activation effects their genes and metabolism, 
+    causing them to lose their ability to multiply (checkpoints) and kill cancer cells. 
+4. Tumors steal vital nutrients from the locality, starving T-cells. 
 """
 # ----------------------- #
 
