fef0faf6e7f7b7bb2943be1840ef5ab9f87092d5
Mikail.U <43473825+mikail2004@users.noreply.github.com>
2026-08-15 23:16:06 -0400

    Setup data and validation

--- data_analysis.py
+++ data_analysis.py
@@ -2,7 +2,7 @@
 # Tumor T-Cell Action
 # Mikail U.
 
-# ----------------------- #
+# ----------- INTRODUCTION ------------ #
 # Scanpy – Single-Cell Analysis in Python (GPU version available)
 # Task: Identify non-exhausted T-Cells that can be used against tumors.
 # Data: Series GSE108989 (Colorectal Cancer)
@@ -18,5 +18,19 @@ Notes:
     causing them to lose their ability to multiply (checkpoints) and kill cancer cells. 
 4. Tumors steal vital nutrients from the locality, starving T-cells. 
 """
-# ----------------------- #
 
+# ----------- REQUIREMENTS ------------ #
+import scanpy as sc
+import pandas as pd
+import numpy as np
+
+# Loading data (No un-compression needed)
+raw_data_path = "GSE108989_CRC.TCell.S11138.count.txt.gz"
+raw_data = pd.read_csv(raw_data_path, sep='\t', index_col=0)
+
+# SC Expectation: Transpose so that rows = cells (barcodes), cols = genes 
+adata = sc.AnnData(X=raw_data.T.values, 
+                   obs=pd.DataFrame(index=raw_data.columns), # cells
+                   var=pd.DataFrame(index=raw_data.index)) # genes
+
+print(adata)
